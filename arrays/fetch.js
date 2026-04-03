6c6a8ed484a6b8603e9c9d122f4c425c04c85d1d
AscMar <ascona.martina@gmail.com>
2026-04-03 11:05:07 -0300

    punto 1.b

--- arrays/fetch.js
+++ arrays/fetch.js
@@ -18,5 +18,47 @@ async function personajes() {
         console.log(`error -> ${error}`);
     }
 }
+// Ejercicio 1b
 
+async function NuevoPersonaje() {
+    try{
+        const agregar = {
+            "id": 0,
+            "firstName": "Nuevo",
+            "lastName": "Personaje",
+            "fullName": "Nuevo Personaje",
+            "title": "Agregado",
+            "family": "Los Nuevos",
+            "image": "x.jpg",
+            "imageUrl": "x.com",
+        };
+        const resp = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(agregar)
+        });
+        if (!resp.ok) {
+            console.log('Error. Por favor verifique los datos');
+            return;
+        }
+
+        const datos = await resp.json();
+        console.log('Personaje agregado con exito:', datos);
+
+        } 
+        
+        catch (error) {
+        console.log(`error -> ${error}`);
+        }
+
+    
+}
 personajes();
+NuevoPersonaje();
+
+
+
+
+
