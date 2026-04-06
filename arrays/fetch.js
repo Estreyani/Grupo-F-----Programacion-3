9dc17c9ac63b269b0275b7145c52eb41aa2e8797
Estrella Cardozo <estrellacardozoit@gmail.com>
2026-04-06 11:30:28 -0300

    Se agrega actividad 2d y se crea funcion ResultadoTp para ejecutar el codigo corroborando funcionamiento

--- arrays/fetch.js
+++ arrays/fetch.js
@@ -11,7 +11,7 @@ const rutaPersonajes = path.join(__dirname, 'personajes.json');
 
 function guardarDatos(data) {
     fs.writeFileSync(rutaPersonajes, JSON.stringify(data, null, 2), 'utf-8');
-    console.log('Datos guardados correctamente en personajes.json');
+    console.log('✔✔✔ Datos guardados correctamente en personajes.json');
 }
 
 async function personajes() {
@@ -24,7 +24,6 @@ async function personajes() {
         }
 
         const datos = await resp.json();
-
         guardarDatos(datos);
 
     } catch (error) {
@@ -55,8 +54,7 @@ async function NuevoPersonaje() {
             body: JSON.stringify(agregar)
         });
 
-        console.log("Estado de la respuesta (Status):", resp.status);
-
+        console.log(" ☑ Estado de la respuesta (Status):", resp.status);
 
         if (!resp.ok) {
             console.log('Error. Por favor verifique los datos');
@@ -69,7 +67,7 @@ async function NuevoPersonaje() {
             const datos = JSON.parse(texto);
             console.log('Personaje agregado con exito:', datos);
         } else {
-            console.log('Personaje agregado (sin respuesta en body)');
+            console.log('☑ Personaje agregado (Confirmación recibida sin datos)');
         }
 
     } catch (error) {
@@ -89,7 +87,7 @@ async function buscarPersonaje(id) {
         }
 
         const personaje = await respuesta.json();
-        console.log("Personaje: ", personaje);
+        console.log("🔍︎ Personaje encontrado: ", personaje);
 
     } catch (error) {
         console.log(`Error en busqueda, Por favor intentelo nuevamente -> ${error}`);
@@ -102,20 +100,21 @@ async function agregarAlFinal() {
     try {
         const data = JSON.parse(fs.readFileSync(rutaPersonajes, 'utf-8'));
 
-        console.log("Antes:", data.length);
+        console.log("...Total personajes inicial:", data.length);
+
         const nuevo = {
             id: 99,
-            firstName: "Final",
-            lastName: "Personaje",
-            fullName: "Final Personaje",
-            title: "Agregado",
-            family: "Custom",
-            image: "",
-            imageUrl: ""
+            firstName: "Duncan",
+            lastName: "el Alto",
+            fullName: "Duncan el Alto",
+            title: "Caballero Errante",
+            family: "Casa Targaryen (Leal)",
+            image: "duncan.jpg",
+            imageUrl: "https://thronesapi.com/assets/images/duncan.jpg"
         };
 
         data.push(nuevo);
-        console.log("Después:", data.length);
+        console.log("...Total personajes final:", data.length);
 
         fs.writeFileSync(rutaPersonajes, JSON.stringify(data, null, 2), 'utf-8');
         console.log('✔ Personaje agregado al final');
@@ -131,33 +130,33 @@ async function agregarDosAlInicio() {
     try {
         const data = JSON.parse(fs.readFileSync(rutaPersonajes, 'utf-8'));
 
-        console.log("Antes:", data.length);
+        console.log("...Total personajes inicial:", data.length);
 
         const nuevo1 = {
             id: 200,
-            firstName: "Primero",
-            lastName: "Inicio",
-            fullName: "Primero Inicio",
-            title: "Agregado",
-            family: "Custom",
-            image: "",
-            imageUrl: ""
+            firstName: "Aegon",
+            lastName: "Targaryen",
+            fullName: "Aegon V (Egg)",
+            title: "Escudero",
+            family: "Casa Targaryen",
+            image: "egg.jpg",
+            imageUrl: "https://thronesapi.com/assets/images/egg.jpg"
         };
 
         const nuevo2 = {
             id: 201,
-            firstName: "Segundo",
-            lastName: "Inicio",
-            fullName: "Segundo Inicio",
-            title: "Agregado",
-            family: "Custom",
-            image: "",
-            imageUrl: ""
+            firstName: "Baelor",
+            lastName: "Targaryen",
+            fullName:"Baelor Rompelanza",
+            title: "Príncipe de Rocadragón",
+            family: "Casa Targaryen",
+            image: "baelor.jpg",
+            imageUrl: "https://thronesapi.com/assets/images/baelor.jpg"
         };
 
         data.unshift(nuevo1, nuevo2);
 
-        console.log("Después:", data.length);
+        console.log("...Total personajes final:", data.length);
         fs.writeFileSync(rutaPersonajes, JSON.stringify(data, null, 2), 'utf-8');
 
         console.log('✔ Dos personajes agregados al inicio');
@@ -173,32 +172,63 @@ async function eliminarPersonaje() {
     try {
         const data = JSON.parse(fs.readFileSync(rutaPersonajes, 'utf-8'));
 
-        const eliminado= data[0]; 
-        console.log('Eliminando primer personaje:', eliminado);
+        const eliminado = data[0];
+        console.log('🗑 Eliminando primer personaje:', eliminado);
+        
         data.splice(0, 1);
-        console.log("Personaje eliminado con exito");
+        console.log("...Personaje eliminado con exito...");
 
-    fs.writeFileSync(rutaPersonajes, JSON.stringify(data, null, 2), 'utf-8');
+        fs.writeFileSync(rutaPersonajes, JSON.stringify(data, null, 2), 'utf-8');
 
     } catch (error) {
         console.log(`Error -> ${error}`);
     }
 }
 
-// Traer todos (y guardar JSON)
-personajes();
+/////////////////// Ejercicio 2d ////////////////////
 
-// Crear uno nuevo
-NuevoPersonaje();
+async function personajesId() {
+    try {
+        const archivo = fs.readFileSync(rutaPersonajes, "utf-8");
+        const personajes = JSON.parse(archivo);
 
-// Buscar por ID
-buscarPersonaje(12); 
+        const nuevoArray = [];
 
-// Agregar al final del JSON
-agregarAlFinal();
+        for (let i = 0; i < personajes.length; i++) {
+            const personaje = personajes[i];
 
-// Agregar dos personajes al inicio
-agregarDosAlInicio();
+            nuevoArray.push({
+                id: personaje.id,
+                nombre: personaje.fullName
+            });
+        }
 
-// Eliminar
-eliminarPersonaje();
+        const rutaIdYNombre = path.join(__dirname, 'personajesId.json');
+        fs.writeFileSync(rutaIdYNombre, JSON.stringify(nuevoArray, null, 2), "utf-8");
+
+        console.log("✔✔✔ Archivo personajesId.json creado con éxito");
+        console.log(nuevoArray);
+
+    } catch (error) {
+        console.log("Error en el creacion de archivo personajesId.json: ", error);
+    }
+}
+
+//////Funcion de flujo de corroboracion de funcionamiento de cada ejercicio//////////
+async function ResultadoTp() {
+    // Se crea archivo personajes.json con los personajes traidos de la api
+    await personajes();         // 1. Trae los personajes y los guarda en personajes.json
+
+    await NuevoPersonaje();     // agrega un nuevo personaje a la Api
+    await buscarPersonaje(12);  // busca personaje por id y lo muestra por consola 
+
+    await agregarAlFinal();     // agrega un nuevo personaje al final a personajes.json
+    await agregarDosAlInicio(); // agrega dos nuevos personajes al inicio a personajes.json
+    await eliminarPersonaje();  // elimina el primer personaje de personajes.json
+
+    // Se crea archivo personajesId.json con nombre y id de cada personaje de personajes.json
+    await personajesId();
+}
+
+ResultadoTp();
+/////////////////////////////////////////////////////////////////////////////////
